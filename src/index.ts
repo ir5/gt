69786ff8d55b249d4d82bb54c428786c010e24a1
Mitsuru Kusumoto <kusumoto.mitsuru@gmail.com>
2020-09-28 23:01:35 +0900

    chain and bonus

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,54 @@ function simulateFall(field: Int8Array) {
   }
 }
 
+function simulateErase(field: Int8Array, chain: number): number {
+  // chain should be >= 1
+  let visited = new Int8Array(13 * 6);
+  const chain_bonus_list = [0, 8, 16, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512];
+  const group_bonus_list = [0, 2, 3, 4, 5, 6, 7, 10];
+  const color_bonus_list = [0, 0, 3, 6, 12];
+
+  function dfs(y: number, x: number, c: number, f: number): number {
+    if (y <= 0 || x < 0 || y > 12 || x >= 6) return 0;
+    const index = y * 6 + x;
+    if (field[index] != c) return 0;
+    if (visited[index] == f) return 0;
+    visited[index] = f;
+    return 1 + dfs(y - 1, x, c, f) + dfs(y + 1, x, c, f) + dfs(y, x - 1, c, f) + dfs(y, x + 1, c, f);
+  };
+
+  let colors = new Set();
+  let group_bonus = 0;
+  let total = 0;
+  for (let y = 1; y <= 12; ++y) {
+    for (let x = 0; x < 6; ++x) {
+      let c = field[y * 6 + x];
+      if (c == 0 || visited[y * 6 + x]) continue;
+      const count = dfs(y, x, c, 1);
+      if (count <= 3) continue;
+      group_bonus += group_bonus_list[count - 4];  // FIXME: crush when count >= 12
+      total += count;
+      colors.add(c);
+
+      // mark erase flag
+      dfs(y, x, c, 2);
+    }
+  }
+  const color_bonus = color_bonus_list[colors.size];
+  const chain_bonus = chain_bonus_list[chain - 1];
+
+  const score = 10 * total * Math.max(chain_bonus + group_bonus + color_bonus, 1);
+
+  // perform erasion
+  for (let y = 1; y <= 12; ++y) {
+    for (let x = 0; x < 6; ++x) {
+      if (visited[y * 6 + x] == 2) field[y * 6 + x] = 0;
+    }
+  }
+
+  return score;
+}
+
 function render(field: Int8Array) {
   const RESET = "\033[0m";
   const RED = "\033[0;41m";
@@ -45,13 +93,38 @@ function render(field: Int8Array) {
 
 function main() {
   let field = new Int8Array(13 * 6);
-  field[12 * 6 + 1] = 1;
-  field[10 * 6 + 1] = 1;
-  field[10 * 6 + 2] = 2;
-  field[8 * 6 + 2] = 3;
-  render(field);
-  simulateFall(field);
-  render(field);
+
+  let aaa = [
+    "......",
+    "......",
+    "......",
+    "......",
+    "......",
+    "..3...",
+    "123124",
+    "123124",
+    "124124",
+    "444333",
+    "123124",
+    "123124",
+    "123124",
+  ];
+
+  for (let y = 0; y <= 12; y++) {
+    for (let x = 0; x < 6; x++) {
+      let c = aaa[y].charAt(x);
+      if ('1' <= c && c <= '4') field[y * 6 + x] = parseInt(c);
+    }
+  }
+
+  for (let chain = 1; ; chain++) {
+    simulateFall(field);
+    render(field);
+    let score = simulateErase(field, chain);
+    if (score == 0) break;
+    console.log(score);
+    render(field);
+  }
 }
 
 main();
