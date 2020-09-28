7e3cc75ca5b4b942954c78a37ed447f255d19f57
Mitsuru Kusumoto <kusumoto.mitsuru@gmail.com>
2020-09-29 00:17:41 +0900

    drop

--- src/index.ts
+++ src/index.ts
@@ -66,6 +66,40 @@ function simulateErase(field: Int8Array, chain: number): number {
   return score;
 }
 
+function simulateDrop(field: Int8Array, pair: Int8Array, pos: number, dir: number): boolean {
+  // dir: 0 --> no rotation
+  // dir: 1 --> 90 degree right rotation
+  // dir: 2 --> 180 degree rotation
+  // dir: 3 --> 90 degree left rotation
+  
+  // dir: 0 or 2
+  // a
+  // b
+  // dir: 1 or 3
+  // a b
+
+  if (pos < 0 || pos >= 6) return false;
+  if (dir % 2 == 1 && pos == 5) return false;
+
+  let which = (dir == 0 || dir == 3) ? 0 : 1;
+  const a = pair[which];
+  const b = pair[1 - which];
+
+  if (dir % 2 == 0) {
+    if (field[1 * 6 + pos] != 0) return false;
+    field[0 * 6 + pos] = b;
+    simulateFall(field);
+    field[0 * 6 + pos] = a;
+    simulateFall(field);
+  } else {
+    if (field[0 * 6 + pos] || field[0 * 6 + pos + 1]) return false;
+    field[0 * 6 + pos] = a;
+    field[0 * 6 + pos + 1] = b;
+    simulateFall(field);
+  }
+  return true;
+}
+
 function render(field: Int8Array) {
   const RESET = "\033[0m";
   const RED = "\033[0;41m";
@@ -95,21 +129,22 @@ function main() {
   let field = new Int8Array(13 * 6);
 
   let aaa = [
-    "......",
-    "......",
-    "......",
-    "......",
-    "......",
-    "..3...",
-    "123124",
-    "123124",
-    "124124",
-    "444333",
-    "123124",
-    "123124",
-    "123124",
+    "1.....",
+    "2...43",
+    "1.2223",
+    "441244",
+    "311424",
+    "144233",
+    "133423",
+    "344321",
+    "114311",
+    "123434",
+    "224113",
+    "111344",
+    "444334",
   ];
 
+  /*
   for (let y = 0; y <= 12; y++) {
     for (let x = 0; x < 6; x++) {
       let c = aaa[y].charAt(x);
@@ -117,14 +152,26 @@ function main() {
     }
   }
 
+  let total = 0;
   for (let chain = 1; ; chain++) {
     simulateFall(field);
     render(field);
     let score = simulateErase(field, chain);
     if (score == 0) break;
-    console.log(score);
+    total += score;
+    console.log(total);
     render(field);
   }
+  */
+
+  let pairs = new Int8Array(8);
+  pairs[0] = 1;
+  pairs[1] = 2;
+  simulateDrop(field, pairs.slice(0, 2), 4, 3);
+  simulateDrop(field, pairs.slice(0, 2), 4, 3);
+  simulateDrop(field, pairs.slice(0, 2), 4, 3);
+  simulateDrop(field, pairs.slice(0, 2), 4, 3);
+  render(field);
 }
 
 main();
