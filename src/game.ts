58f46cf53d4a03c858310adc21b99c698653941b
Mitsuru Kusumoto <kusumoto.mitsuru@gmail.com>
2020-09-29 21:10:29 +0900

    game class

---
+++ src/game.ts
@@ -0,0 +1,75 @@
+import {Act, simulateFall, simulateErase, simulateDrop} from "./simulation"
+
+export type Agent = (field: Int8Array, pairs: Int8Array) => Act;
+
+export class Game {
+  field: Int8Array;
+  score: number;
+  turn: number;
+  pairs: Int8Array;
+
+  constructor(pairs: Int8Array) {
+    this.field = new Int8Array(13 * 6);
+    this.score = 0;
+    this.turn = 0;
+    this.pairs = pairs;
+  }
+
+  render() {
+    const RESET = "\x1b[0m";
+    const RED = "\x1b[0;41m";
+    const GREEN = "\x1b[0;42m";
+    const BLUE = "\x1b[0;44m";
+    const YELLOW = "\x1b[0;43m";
+
+    function conv(c: number) {
+      if (c == 0) return ".";
+      if (c == 1) return RED + "R" + RESET;
+      if (c == 2) return GREEN + "G" + RESET;
+      if (c == 3) return BLUE + "B" + RESET;
+      if (c == 4) return YELLOW + "Y" + RESET;
+      return "";
+    }
+
+    console.log("");
+    console.log("########")
+    for (let y = 0; y < 13; y++) {
+      let line: string = "#";
+      for (let x = 0; x < 6; x++) {
+        let c = this.field[y * 6 + x];
+        line += conv(c);
+      }
+      line += "#";
+
+      const nextys = [1, 2, 4, 5, 7, 8];
+      for (let i = 0; i < 6; i++) {
+        if (y == nextys[i]) line += "  " + conv(this.pairs[this.turn * 2 + i]);
+      }
+
+      console.log(line);
+      if (y == 0) console.log("#------#")
+    }
+    console.log("########")
+  }
+
+  run(agent: Agent) {
+    this.render();
+
+    for (; this.turn * 2 + 6 < this.pairs.length; ) {
+      const i = this.turn * 2;
+      const act = agent(this.field, this.pairs.slice(i, i + 6));
+      simulateDrop(this.field, this.pairs.slice(i, i + 2), act);
+      this.turn++;
+      this.render();
+      for (let chain = 1; ; chain++) {
+        const score_gain = simulateErase(this.field, chain);
+        if (score_gain == 0) break;
+        this.score += score_gain;
+        this.render();
+
+        simulateFall(this.field);
+        this.render();
+      }
+    }
+  }
+}
---
+++ src/simulation.ts
@@ -0,0 +1,104 @@
+export type Act = [number, number];  // [left pos, direction]
+// direction: 0 --> no rotation
+// direction: 1 --> 90 degree right rotation
+// direction: 2 --> 180 degree rotation
+// direction: 3 --> 90 degree left rotation
+  
+
+// field is represented by Int8Array of 13*6 elements.
+// A cell at the i-th row from top in the j-th column from left is field[i*6+j].
+// When element == 0, it means an empty cell.
+// When element >= 1, the cell is occupied by a puyo.
+
+export function simulateFall(field: Int8Array) {
+  for (let x = 0; x < 6; x++) {
+    let bottom = 12;
+    for (let y = 12; y >= 0; y--) {
+      const index = y * 6 + x;
+      if (field[index] > 0) {
+        const val = field[index];
+        field[index] = 0;
+        field[bottom * 6 + x] = val;
+        bottom--;
+      }
+    }
+  }
+}
+
+export function simulateErase(field: Int8Array, chain: number): number {
+  // chain should be >= 1
+  let visited = new Int8Array(13 * 6);
+  const chain_bonus_list = [0, 8, 16, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512];
+  const group_bonus_list = [0, 2, 3, 4, 5, 6, 7, 10];
+  const color_bonus_list = [0, 0, 3, 6, 12];
+
+  function dfs(y: number, x: number, c: number, f: number): number {
+    if (y <= 0 || x < 0 || y > 12 || x >= 6) return 0;
+    const index = y * 6 + x;
+    if (field[index] != c) return 0;
+    if (visited[index] == f) return 0;
+    visited[index] = f;
+    return 1 + dfs(y - 1, x, c, f) + dfs(y + 1, x, c, f) + dfs(y, x - 1, c, f) + dfs(y, x + 1, c, f);
+  };
+
+  let colors = new Set();
+  let group_bonus = 0;
+  let total = 0;
+  for (let y = 1; y <= 12; ++y) {
+    for (let x = 0; x < 6; ++x) {
+      let c = field[y * 6 + x];
+      if (c == 0 || visited[y * 6 + x]) continue;
+      const count = dfs(y, x, c, 1);
+      if (count <= 3) continue;
+      group_bonus += group_bonus_list[count - 4];  // FIXME: crush when count >= 12
+      total += count;
+      colors.add(c);
+
+      // mark erase flag
+      dfs(y, x, c, 2);
+    }
+  }
+  const color_bonus = color_bonus_list[colors.size];
+  const chain_bonus = chain_bonus_list[chain - 1];
+
+  const score = 10 * total * Math.max(chain_bonus + group_bonus + color_bonus, 1);
+
+  // perform erasion
+  for (let y = 1; y <= 12; ++y) {
+    for (let x = 0; x < 6; ++x) {
+      if (visited[y * 6 + x] == 2) field[y * 6 + x] = 0;
+    }
+  }
+
+  return score;
+}
+
+export function simulateDrop(field: Int8Array, pair: Int8Array, act: Act): boolean {
+  const [pos, dir] = act;
+  // dir: 0 or 2
+  // a
+  // b
+  // dir: 1 or 3
+  // a b
+
+  if (pos < 0 || pos >= 6) return false;
+  if (dir % 2 == 1 && pos == 5) return false;
+
+  let which = (dir == 0 || dir == 3) ? 0 : 1;
+  const a = pair[which];
+  const b = pair[1 - which];
+
+  if (dir % 2 == 0) {
+    if (field[1 * 6 + pos] != 0) return false;
+    field[0 * 6 + pos] = b;
+    simulateFall(field);
+    field[0 * 6 + pos] = a;
+    simulateFall(field);
+  } else {
+    if (field[0 * 6 + pos] || field[0 * 6 + pos + 1]) return false;
+    field[0 * 6 + pos] = a;
+    field[0 * 6 + pos + 1] = b;
+    simulateFall(field);
+  }
+  return true;
+}
